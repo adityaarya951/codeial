c07b11c6be82dc97309fa5ff663013d29e3dc22f
Aditya Arya <adityaarya@Adityas-MacBook-Air.local>
2023-09-25 00:02:23 +0530

    created a css file and added ion layout

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-//^ use of `` is called interpolarion , using interpolation u can keep writing the string 
-//^ and then u can use ${} fro anything that needs to be evaluated , ${} from this browser ubderstands 
-//^ that something has come to be evaluated 
+const expresslayouts = require('express-ejs-layouts');
+//^ hum layouts ka use kar rahe hain to jo bhi humari repeating cheeze hain wo layouts me 
+//^ rahengi , jaise footer , header , hume files me sirf variable part likhna hai 
+//^ body waala baaki sab apne aap layout.ejs se aa jaega 
+app.use(expresslayouts);
+
+//^ express.static is used to acess all the static files 
+//^ these static files has media, js , css
+app.use(express.static('./assets'))
+
 
 //^ we will be using express router for managing all our routes
 //^ humari koi bhi req hogi wo seedha routes pe jaegi 
 app.use('/', require('./routes'));
+
 //^ when we have case sensitive routes we use app.set , else app.use
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//^ use of `` is called interpolarion , using interpolation u can keep writing the string 
+//^ and then u can use ${} fro anything that needs to be evaluated , ${} from this browser ubderstands 
+//^ that something has come to be evaluated 
 app.listen(port, function(err){
     if(err){
         console.log(`error in running the server: ${err}`);
